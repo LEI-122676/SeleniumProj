b6471e08908d57846e53f3dfe03bd8c417995b02
LEI-122676 <tfcss2@iscte-iul.pt>
2025-11-23 19:14:10 +0000

    SecureDone

---
+++ src/test/java/secure/BaseChromeTest.java
@@ -0,0 +1,11 @@
+package secure;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseChromeTest extends BaseTest {
+
+  @BeforeEach
+  public void setup() {
+    startChromeDriver();
+  }
+}
