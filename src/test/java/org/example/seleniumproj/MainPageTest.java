79cf6dd72e423486ed73a48d607c1c789bdae7cf
Eduardo Correia <edrca@iscte-iul.pt>
2025-11-28 14:49:52 +0000

    validação dos testes ELements dropdown e checkbox

--- src/test/java/org/example/seleniumproj/MainPageTest.java
+++ src/test/java/org/example/seleniumproj/MainPageTest.java
@@ -14,7 +14,6 @@ import java.time.Duration;
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
-    private WebDriverWait wait;
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
@@ -48,26 +47,30 @@ public class MainPageTest {
     public void search() {
         mainPage.searchButton.click();
 
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test-id='search-input']")); // Alterei de data-test='search-input' para data-test-id='search-input'
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test-id='search-input']"));
+        // Alterei de data-test='search-input' para data-test-id='search-input'
         searchField.sendKeys("Selenium");
 
         WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
         submitButton.click();
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test-id='search-input']")); // Alterei de data-test='search-input' para data-test-id='search-input'
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test-id='search-input']"));
+        // Alterei de data-test='search-input' para data-test-id='search-input'
         assertEquals("Selenium", searchPageField.getAttribute("value"));
     }
 
     @Test
     public void toolsMenu() {
         mainPage.toolsMenu.click();
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-menu-item']")); // Alterei de data-test='main-submenu' para data-test='main-menu-item'
+        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-menu-item']"));
+        // Alterei de data-test='main-submenu' para data-test='main-menu-item'
         assertTrue(menuPopup.isDisplayed());
     }
     @Test
      public void navigationToAllTools() {
         mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click(); // alterei no MainPage.java o data-test de suggestion-action para suggestion-link
+        mainPage.findYourToolsButton.click();
+        // Alterei no MainPage.java o data-test de suggestion-action para suggestion-link
 
         WebElement productsList = driver.findElement(By.id("products-page"));
         assertTrue(productsList.isDisplayed());
